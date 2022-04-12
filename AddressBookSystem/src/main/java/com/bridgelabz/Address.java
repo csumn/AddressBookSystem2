d70bde6dbfb671c3fe5063c96c28a633a511d7cf
csumn <csumn13@gmail.com>
2022-04-12 16:40:26 +0530

    Uc1

---
+++ AddressBookSystem/src/main/java/com/bridgelabz/Address.java
@@ -0,0 +1,37 @@
+package com.bridgelabz;
+
+public class Address {
+	private String city; 
+	private String state; 
+	private long zip;
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public long getZip() {
+		return zip;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
+
+}
